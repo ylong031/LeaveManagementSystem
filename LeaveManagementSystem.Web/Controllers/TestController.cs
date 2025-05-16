d46f77c48604573534332d70964e5acb17ca0915
ylong031 <140056328+ylong031@users.noreply.github.com>
2025-05-17 06:15:14 +0800

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            /*var data = new TestViewModel
+            {
+                Name = "Student"
+            };*/
+
+            var data = new TestViewModel();
+            data.Name = "Student";
+            data.DateOfBirth = new DateTime(1954, 12, 01);
+
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+       /*  ? -> nullable.optional.
+         It can hold a value or not
+       */
+
+    }
+}
