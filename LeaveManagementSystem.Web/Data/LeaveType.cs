65058de8f98f94bf35561231b79ee5e2a0027f80
ylong031 <140056328+ylong031@users.noreply.github.com>
2025-05-18 04:32:40 +0800

    Adding LeaveType table to database

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        //if you use "Id" it will automatically become your primary key
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+    }
+}
